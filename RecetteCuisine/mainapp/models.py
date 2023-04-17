1c69b8abe9b718bc2b7b789de041491abd1dd2af
Boutissante <73436159+issamBoutissante@users.noreply.github.com>
2023-04-17 10:58:52 +0200

    Models Created

--- RecetteCuisine/mainapp/models.py
+++ RecetteCuisine/mainapp/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    label = models.CharField(max_length=50)
+
+
+class Recipe(models.Model):
+    title = models.TextField
+    description = models.TextField
+    duration = models.IntegerField
+    nbrPersons = models.IntegerField
+    nbrCalories = models.IntegerField
+    difficulty = models.IntegerField
+    categories = models.ManyToManyField(Category)
+
+
+class Ingredient(models.Model):
+    description = models.TextField
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+
+class Step(models.Model):
+    descreption = models.TextField
+    number = models.IntegerField
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+
+class User(models.Model):
+    email = models.TextField
+    password = models.TextField
+
+
+class Comment(models.Model):
+    text = models.TextField
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+
+class Rating(models.Model):
+    rate = models.IntegerField
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
